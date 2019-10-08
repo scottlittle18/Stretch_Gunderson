b349322eedc5d282366e27bd63cafe6b34bb5045
Scott Little <scott.little@loop.colum.edu>
2019-10-08 11:07:12 -0500

    Added Project

---
+++ Assets/Scripts/Extendable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///     This Parent class should be used by the parts of the character that the
+/// player will be able to control
+/// </summary>
+public class Extendable : MonoBehaviour
+{
+    protected class DefaultScaleSettings
+    {
+        // Default Scale Settings
+        public float yScale;
+
+        /// <summary>
+        ///  Simple Constructor for YAxis ONLY
+        /// </summary>
+        /// <param name="yAxis"> This should be the transform.localScale.y of the child object</param>
+        public DefaultScaleSettings(float yAxis)
+        {
+            yScale = yAxis;
+        }
+    }
+}
---
+++ Assets/Scripts/IExtendable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IExtendable
+{
+    //Required Properties
+    float MaxExtensionLimit { get; set; }
+
+    //Required Methods
+    void Extend();
+    void ShrinkToOriginalSize();
+}
