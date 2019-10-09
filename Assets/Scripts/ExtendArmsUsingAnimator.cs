1041f4b6d2a89447450d1e04422f8baea94548a6
Scott Little <scott.little@loop.colum.edu>
2019-10-09 18:38:01 -0500

    Attempting arm rotation input via axis input

--- Assets/Scripts/ExtendArmsUsingAnimator.cs
+++ Assets/Scripts/ExtendArmsUsingAnimator.cs
@@ -8,28 +8,8 @@ using UnityEngine;
 /// </summary>
 public class ExtendArmsUsingAnimator : Extendable, IExtendable
 {
-    //#region --Serialized Fields--
-    //[SerializeField]
-    //private float bouncinessOnCollision;
-
-    //[SerializeField]
-    //private PhysicsMaterial2D armsPhysMat;
-
-    //[SerializeField, Tooltip("Mass of the arm when it's in its' Idle Animation State")]
-    //private float startingMass;
-
-    //[SerializeField, Tooltip("Mass of the arm when it's in its' Arm_KeepExtendedArm Animation State")]
-    //private float extensionMass;
-    //#endregion
-
-    //#region Non-Serialized Fields
-    //// Unity Classes
-    //private Animator armAnim;
-    //private Rigidbody2D armRigidbody;
-
     // Input Variables
     private bool extendObjectInputActive;
-    //#endregion
 
     // Start is called before the first frame update
     void Start()
@@ -41,8 +21,11 @@ public class ExtendArmsUsingAnimator : Extendable, IExtendable
     void Update()
     {
         ListenForInput();
-        base.MassController();
+        base.MassController();        
+    }
 
+    private void ListenForInput()
+    {
         //TODO: Change the name Jump to ExtendArms in Unity && Here
         extendObjectInputActive = Input.GetButton("Jump");
 
@@ -57,64 +40,12 @@ public class ExtendArmsUsingAnimator : Extendable, IExtendable
         }
     }
 
-    public virtual void ListenForInput()
-    {
-        
-    }
-
-    /// <summary>
-    /// Controls the mass of the Arms to increase or decrease the amount of 
-    ///  force applied to the rest of the Player
-    /// </summary>
-    //public override void MassController()
-    //{
-    //    // If extending, Then increase mass to increase force applied to the rest of the Player
-    //    if (armAnim.GetBool("Extend") == true)
-    //    {
-    //        armRigidbody.mass = extensionMass;
-    //    }
-    //    else if (armAnim.GetBool("Extend") == false)
-    //    {
-    //        armRigidbody.mass = startingMass;
-    //    }
-    //}
-
-    /// <summary>
-    /// Listen for Input from the Player
-    /// </summary>
-    //public override void ListenForInput()
-    //{
-    //    //TODO: Change the name Jump to ExtendArms in Unity && Here
-    //    extendingArms = Input.GetButtonDown("Jump");
-
-    //    if (extendingArms)
-    //    {
-    //        Extend();
-    //    }
-
-    //    if (!extendingArms)
-    //    {
-    //        ResetScale();
-    //    }
-    //}
-
-    //public override void Extend()
-    //{
-    //    armAnim.SetBool("Extend", true);
-    //}
-
-    //public override void ResetScale()
-    //{
-    //    armAnim.SetBool("Extend", false);
-    //}
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
             // Make the Collider on the arm bouncy right before hitting the ground to add a little extra push
             extendableObjectPhysMat.bounciness = bouncinessOnCollision;
-            Debug.Log($"Player's Arm Material Has converted to: {extendableObjectPhysMat.bounciness}");
         }
     }
 
@@ -124,7 +55,6 @@ public class ExtendArmsUsingAnimator : Extendable, IExtendable
         {
             // Stop arm from being bouncy when it leaves the ground
             extendableObjectPhysMat.bounciness = 0;
-            Debug.Log($"Player's Arm Material Has reverted to: {extendableObjectPhysMat.bounciness}");
         }
     }
 }
